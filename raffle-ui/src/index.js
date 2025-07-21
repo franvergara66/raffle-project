427e3023f9ae559ef43b33f61b32cb9fe8fa22ea
Francisco Vergara <fvergara.sand@gmail.com>
2025-07-21 14:08:23 +0200

    Load admin app styles

--- raffle-ui/src/index.js
+++ raffle-ui/src/index.js
@@ -13,7 +13,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles/admin.css';
-import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
