1f4657ff8618e58eefd30562394a31fbe68d31f2
Francisco Vergara <fvergara.sand@gmail.com>
2025-07-30 16:29:57 +0200

    Increase JSON body size limit

--- raffle-draw-api/app/middleware.js
+++ raffle-draw-api/app/middleware.js
@@ -2,6 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const middleware = [morgan("dev"), cors(), express.json()];
+const middleware = [
+  morgan("dev"),
+  cors(),
+  express.json({ limit: "10mb" }),
+  express.urlencoded({ extended: true, limit: "10mb" }),
+];
 
 module.exports = middleware;
