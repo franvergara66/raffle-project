a90c4d488882b67d0f0f08762f0c01d912453cb7
Francisco Vergara <fvergara.sand@gmail.com>
2025-07-31 12:01:32 +0200

    add changes manually

--- raffle-ui/src/components/Recaptcha.js
+++ raffle-ui/src/components/Recaptcha.js
@@ -1,31 +1,37 @@
 import React, { useEffect } from 'react';
 
-const Recaptcha = ({ onVerify }) => {
+/**
+ * Simple wrapper to load and render Google reCAPTCHA.
+ * Explicitly renders the captcha once the script has loaded.
+ */
+const Recaptcha = ({ siteKey, onVerify }) => {
   useEffect(() => {
     const scriptId = 'recaptcha-script';
+
+    const renderCaptcha = () => {
+      if (window.grecaptcha && siteKey) {
+        window.grecaptcha.render('recaptcha-container', {
+          sitekey: siteKey,
+          callback: onVerify,
+        });
+      }
+    };
+
     if (!document.getElementById(scriptId)) {
       const script = document.createElement('script');
       script.id = scriptId;
-      script.src = 'https://www.google.com/recaptcha/api.js';
+      script.src = 'https://www.google.com/recaptcha/api.js?render=explicit';
       script.async = true;
       script.defer = true;
+      script.onload = renderCaptcha;
       document.body.appendChild(script);
+    } else {
+      // script already loaded, try rendering immediately
+      renderCaptcha();
     }
-    window.verifyRecaptchaCallback = (token) => {
-      if (onVerify) onVerify(token);
-    };
-    return () => {
-      delete window.verifyRecaptchaCallback;
-    };
-  }, [onVerify]);
+  }, [siteKey, onVerify]);
 
-  return (
-    <div
-      className="g-recaptcha"
-      data-sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
-      data-callback="verifyRecaptchaCallback"
-    ></div>
-  );
+  return <div id="recaptcha-container" />;
 };
 
 export default Recaptcha;
